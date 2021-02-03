2eb36eea1c00a0251e5a483bb7c8be5a89794c38
Sam Girshovich <sam.girshovich@gmail.com>
2021-02-03 12:49:58 -0500

    OPEN SOURCE LINK AND CAPS

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Location } from './location';
 import { Support } from './support';
+import { Link } from '@material-ui/core';
 
 function App() {
   return (
     <div className="App">
-      <h1>Weatherjet</h1>
-      <h3>The open source Weather App for the United States</h3>
+      <h1>WEATHERJET</h1>
+      
+      <h3>THE <Link href="https://github.com/samg11/weatherjet" target="_blank">OPEN SOURCE</Link> WEATHER APP FOR THE UNITED STATES</h3>
       <Location />
       
       <div className="footer">
